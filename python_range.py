d18bd9b95545f9f21058803cdff729d16bad77af
avinash-03 <avinashgodbole100@gmail.com>
2023-04-06 10:46:28 +0530

    square function added

--- python_range.py
+++ python_range.py
@@ -1,4 +1,9 @@
 start = 1
 end = 15
-for num in range(1,15):
-  print(f"the squre of {num} is {num*num}")
+for num in range(1, 16):
+    print(f"the squre of {num} is {num*num}")
+
+
+# square function
+def square(num):
+    return num * num
